ebc66bb9f1dcd7882521bf8f626de34cd1b82ea5
Nikola <ulsxl@student.kit.edu>
2023-04-04 19:52:27 +0200

    build window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use glium::glutin;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = glutin::event_loop::EventLoop::new();
+    let window_builder = glutin::window::WindowBuilder::new()
+        .with_title("Title")
+        .with_resizable(false)
+        .with_inner_size(glutin::dpi::LogicalSize::new(1024, 576));
+    let _context = glutin::ContextBuilder::new()
+        .build_windowed(window_builder, &event_loop).unwrap();
+
+    event_loop.run(move |ev, _, control_flow| {
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            _ => (),
+        }
+    });
+
 }
