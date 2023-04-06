5b18a173e6675f0205b679df1e31ac7facdffa2f
Nikola <ulsxl@student.kit.edu>
2023-04-06 17:05:13 +0200

    add dependency, add TODO

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
 use glium::{glutin, Surface};
 
 fn main() {
+    // Window and context creation
     let event_loop = glutin::event_loop::EventLoop::new();
-    let window_builder = glutin::window::WindowBuilder::new()
-        .with_title("Title")
-        .with_resizable(false)
+    let window_builder = glutin::window::WindowBuilder::new().with_title("Title").with_resizable(false)
         .with_inner_size(glutin::dpi::LogicalSize::new(1024, 576));
     let context = glutin::ContextBuilder::new();
     let display = glium::Display::new(window_builder, context, &event_loop).unwrap();
 
+    // TODO: load file
+
+    // TODO: extract data
+
+    // TODO: create glsl program
+
     event_loop.run(move |ev, _, control_flow| {
 
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        target.clear_color(0.1, 0.05, 0.1, 1.0);
+
+        // TODO: draw
+
         target.finish().unwrap();
 
-        let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
+        // TODO: add camera control
+        let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
         match ev {
             glutin::event::Event::WindowEvent { event, .. } => match event {
