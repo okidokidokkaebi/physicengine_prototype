900f0f0565c09eace36939f9f85f8ed1681a0d55
Nikola <ulsxl@student.kit.edu>
2023-04-07 15:46:43 +0200

    add projection matrix

--- src/shader/simple_vertex.glsl
+++ src/shader/simple_vertex.glsl
@@ -2,10 +2,11 @@
 
 uniform mat4 model;
 uniform mat4 view;
+uniform mat4 projection;
 
 in vec3 position;
 in vec3 normal;
 
 void main() {
-    gl_Position = view * model * vec4(position, 1.0);
+    gl_Position = projection * view * model * vec4(position, 1.0);
 }
