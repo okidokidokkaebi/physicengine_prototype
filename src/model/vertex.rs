4f3fe2af61a517f34e329d37ce0c94afe8cdc8e2
Nikola <ulsxl@student.kit.edu>
2023-04-07 20:22:54 +0200

    add debug cube

--- src/model/vertex.rs
+++ src/model/vertex.rs
@@ -32,4 +32,41 @@ impl Vert3D {
         println!("vertices length : {}\tindices length : {}", vertices.len(), indices.len());
         return (vertices, indices.concat());
     }
+
+    pub fn from_debug() -> (Vec<Vert3D>, Vec<u32>) {
+    	
+        let vertices: Vec<Vert3D> = vec![
+            Vert3D { position: [-1.0, -1.0, -1.0], normal: [-1.0, -1.0, -1.0] },
+            Vert3D { position: [-1.0, -1.0, 1.0], normal: [-1.0, -1.0, 1.0] },
+            Vert3D { position: [-1.0, 1.0, -1.0], normal: [-1.0, 1.0, -1.0] },
+            Vert3D { position: [-1.0, 1.0, 1.0], normal: [-1.0, 1.0, 1.0] },
+            Vert3D { position: [1.0, -1.0, -1.0], normal: [1.0, -1.0, -1.0] },
+            Vert3D { position: [1.0, -1.0, 1.0], normal: [1.0, -1.0, 1.0] },
+            Vert3D { position: [1.0, 1.0, -1.0], normal: [1.0, 1.0, -1.0] },
+            Vert3D { position: [1.0, 1.0, 1.0], normal: [1.0, 1.0, 1.0] },
+        ];
+
+        let indices: Vec<u32> = vec![
+
+            2, 1, 0,
+            3, 2, 1,
+
+            6, 5, 4, 
+
+            5, 6, 7,
+
+            6, 3, 2, 
+            7, 6, 3,
+
+            4, 1, 0,
+            5, 4, 1,
+
+            5, 3, 1,
+            7, 5, 3,
+
+            4, 2, 0,
+            6, 4, 2,
+        ];
+        return (vertices, indices);
+    }
 }
