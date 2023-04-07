b3a4dcd073b140a69012815e5f44c8c6969736b9
Nikola <ulsxl@student.kit.edu>
2023-04-07 17:00:54 +0200

    change shader

--- src/shader/simple_fragment.glsl
+++ src/shader/simple_fragment.glsl
@@ -1,7 +1,12 @@
 #version 460
 
+uniform vec3 camera_position;
+
 out vec4 color;
 
+in vec4 mv_position;
+in vec4 mv_normal;
+
 void main() {
-    color = vec4(1.0);
+    color = vec4(1.0) * dot(normalize(camera_position - mv_position.xyz), normalize(mv_normal.xyz));
 }
--- src/shader/simple_vertex.glsl
+++ src/shader/simple_vertex.glsl
@@ -7,6 +7,11 @@ uniform mat4 projection;
 in vec3 position;
 in vec3 normal;
 
+out vec4 mv_normal;
+out vec4 mv_position;
+
 void main() {
     gl_Position = projection * view * model * vec4(position, 1.0);
+    mv_normal = transpose(inverse(view * model)) * vec4(normal, 0.0);
+    mv_position = view * model * vec4(position, 1.0);
 }
