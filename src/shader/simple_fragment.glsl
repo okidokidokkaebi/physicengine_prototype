eb2ab1a05c0c27c24e04c539cfbead3058624ac3
Nikola <ulsxl@student.kit.edu>
2023-04-07 18:36:39 +0200

    .

--- src/shader/simple_fragment.glsl
+++ src/shader/simple_fragment.glsl
@@ -4,9 +4,8 @@ uniform vec3 camera_position;
 
 out vec4 color;
 
-in vec4 mv_position;
-in vec4 mv_normal;
+in vec4 fColor;
 
 void main() {
-    color = vec4(1.0) * dot(normalize(camera_position - mv_position.xyz), normalize(mv_normal.xyz));
+    color = fColor;
 }
--- src/shader/simple_vertex.glsl
+++ src/shader/simple_vertex.glsl
@@ -4,14 +4,20 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+uniform vec3 camera_position;
+
 in vec3 position;
 in vec3 normal;
 
-out vec4 mv_normal;
-out vec4 mv_position;
+out vec4 fColor;
 
 void main() {
     gl_Position = projection * view * model * vec4(position, 1.0);
-    mv_normal = transpose(inverse(view * model)) * vec4(normal, 0.0);
-    mv_position = view * model * vec4(position, 1.0);
+    vec4 m_normal = transpose(inverse(model)) * vec4(normal, 0.0);
+    vec4 m_position = model * vec4(position, 1.0);
+
+    vec4 ambient = vec4(0.3, 0.1, 0.6, 1.0);
+    vec4 diffuse = vec4(1.0, 1.0, 1.0, 1.0) * max(dot(normalize(camera_position - m_position.xyz), normalize(m_normal.xyz)), 0.0);
+
+    fColor = ambient + diffuse;
 }
