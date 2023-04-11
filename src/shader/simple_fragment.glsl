0b975814cf072b4ad1032577082155e95592ba9c
Nikola <ulsxl@student.kit.edu>
2023-04-11 13:56:18 +0200

    cube now changes color if colliding

--- src/shader/simple_fragment.glsl
+++ src/shader/simple_fragment.glsl
@@ -1,15 +1,16 @@
 #version 460
 
+uniform vec3 diffuse_input; 
 uniform vec3 camera_position;
-
-out vec4 color;
 
 in vec4 m_normal;
 in vec4 m_position;
 
+out vec4 color;
+
 void main() {
-    vec4 ambient = vec4(0.0, 0.1, 0.7, 1.0);
-    vec4 diffuse = vec4(vec3(0.7, 0.0, 0.0) 
+    vec4 ambient = vec4(0.1, 0.0, 0.0, 1.0);
+    vec4 diffuse = vec4(diffuse_input
         * ( max(dot(normalize(camera_position - m_position.xyz/m_position.w), normalize(m_normal.xyz)), 0.0)), 1.0);
 
     color = ambient + diffuse;
